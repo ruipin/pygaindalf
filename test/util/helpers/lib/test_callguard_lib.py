76f20929625c8138b4dae7e83466baa9944795a3
Rui Pinheiro <rui@ruipinheiro.eu>
2025-09-20 21:26:13 +0100

    Include missing test file and upgrade dependencies

---
+++ test/util/helpers/lib/test_callguard_lib.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPLv3-or-later
+# Copyright © 2025 pygaindalf Rui Pinheiro
+
+from app.util.callguard import callguard
+
+
+@callguard
+class TestDoubleUnderscore:
+    def normal(self) -> str:
+        return "normal ok"
+
+    def normal2(self) -> str:
+        return "normal2 ok"
+
+    def _single_underscore(self) -> str:
+        return "single underscore ok"
+
+    def __double_underscore(self) -> str:
+        return "double underscore ok"
+
+    def access_double_underscore(self) -> str:
+        # Internal access to double underscore method should be allowed
+        return self.__double_underscore()
